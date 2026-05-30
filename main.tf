3c73d97ce74a2488e66cd79b15657b393072fbd5
jithendrakanagarla2003-cyber <jithendrakanagarla2003@gmail.com>
2026-05-30 10:49:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
+provider "aws" {
+region = "us-east-1"
+}
+
 resource "aws_instance" "name" {
-  ami           = "ami-091138d0f0d41ff90"
-  instance_type = "t2.micro"
+  ami           = "ami-00e801948462f718a"
+  instance_type = "t3.micro"
   tags = {
     Name = "dev-instance"
   }
